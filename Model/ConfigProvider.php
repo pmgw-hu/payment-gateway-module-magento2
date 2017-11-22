7fe285057f7b91bf3524f5287ffa6167a6d9ff26
Hadadi Péter <peter.hadadi@bigfish.hu>
2017-11-22 14:12:06 +0100

    WIP: ConfigProvider refactor

--- Model/ConfigProvider.php
+++ Model/ConfigProvider.php
@@ -59,49 +59,76 @@ class ConfigProvider implements ConfigProviderInterface
      */
     public function getConfig()
     {
-        $scopeConfig = $this->scopeConfig->getValue('payment');
+        $providers = $this->getProviders($this->scopeConfig->getValue('payment'));
 
-        $methods = self::CODE . '_';
-
-        $params = array_filter(
-            $scopeConfig,
-            function ($key) use ($methods) {
-                return strpos($key, $methods) === 0;
-            },
-            ARRAY_FILTER_USE_KEY
-        );
-
-        $providers = array();
-
-        foreach ($params as $key => $param) {
-            if ($param['active']) {
-                $providers[] = array_merge(array('name'=>$key), $scopeConfig[self::CODE], $param);
-            }
-        }
-
-        $used_keys = array();
-
-        foreach ($providers as $provider_key=>$provider) {
-            foreach ($provider as $key=>$val) {
-                $used_keys[$key] = $key;
-            }
-        }
-
-        foreach ($providers as $provider_key=>&$provider) {
-            foreach ($used_keys as $key=>$val) {
-                if (!isset($provider[$key])) {
-                    $provider[$key] = '';
-                }
-            }
-        }
+        $this->unifyProviderConfig($providers);
 
         return [
             'payment' => [
                 self::CODE => [
-                    'providers' => $providers
+                    'providers' => $providers,
                 ]
             ]
         ];
+    }
+
+    /**
+     * @param array $scopeConfig
+     * @return array
+     */
+    protected function getProviders(array $scopeConfig)
+    {
+        $prefix = self::CODE . '_';
+
+        $params = array_filter(
+            $scopeConfig,
+            function (array $data, $key) use ($prefix) {
+                return (strpos($key, $prefix) === 0 && (int)$data['active']);
+            },
+            ARRAY_FILTER_USE_BOTH
+        );
+
+        $providers = array_map(
+            function ($key, array $data) use ($scopeConfig) {
+                return array_merge(
+                    ['name' => $key],
+                    $scopeConfig[self::CODE],
+                    $data
+                );
+            }, array_keys($params), $params);
+
+        return $providers;
+    }
+
+    /**
+     * @param array $providers
+     */
+    protected function unifyProviderConfig(array &$providers)
+    {
+        $keys = $this->collectProviderConfigKeys($providers);
+
+        array_walk($providers, function (array &$provider) use ($keys) {
+            $provider = array_merge($keys, $provider);
+        });
+    }
+
+    /**
+     * @param array $providers
+     * @return array
+     */
+    protected function collectProviderConfigKeys(array $providers)
+    {
+        $keys = [];
+
+        array_walk($providers, function (array $provider) use (&$keys) {
+            foreach (array_keys($provider) as $key) {
+                if (array_key_exists($key, $keys)) {
+                    continue;
+                }
+                $keys[$key] = null;
+            }
+        });
+        return $keys;
     }
 
     /**
