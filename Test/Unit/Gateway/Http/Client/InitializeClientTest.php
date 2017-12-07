00d6946b8839d5f811f61b1311d42310693ada58
Hadadi Péter <peter.hadadi@bigfish.hu>
2017-12-07 08:38:37 +0100

    WIP: add test file headers

--- Test/Unit/Gateway/Http/Client/InitializeClientTest.php
+++ Test/Unit/Gateway/Http/Client/InitializeClientTest.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * BIG FISH Ltd.
+ * http://www.bigfish.hu
+ *
+ * @title      Magento -> Custom Payment Module for BIG FISH Payment Gateway
+ * @category   BigFish
+ * @package    BigFish_Pmgw
+ * @author     BIG FISH Ltd., paymentgateway [at] bigfish [dot] hu
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  Copyright (c) 2017, BIG FISH Ltd.
+ */
 namespace BigFish\Pmgw\Test\Unit\Gateway\Http\Clent;
 
 use BigFish\PaymentGateway;
